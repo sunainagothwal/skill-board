7e5e8faf8ae51e336247ea130105fe24ccbfe73f
sunaina <sunainagothwal271@gmail.com>
2025-08-25 13:10:19 +0530

    code changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,98 +11,7 @@ import SignIn from "./components/auth/SignIn";
 import PrivateRoute from "./PrivateRoute";
 
 function App() {
-  const users = [
-    {
-      name: "Sunaina",
-      city: "Delhi",
-      knows: "JavaScript, React",
-      wants: "Full-Stack Projects",
-      img: "https://randomuser.me/api/portraits/women/68.jpg",
-      description:
-        "A passionate developer eager to build modern web applications.",
-    },
-    {
-      name: "Bhagirath",
-      city: "Bengaluru",
-      knows: "UI/UX Design, React.js",
-      wants: "Front-End Internships",
-      img: "https://randomuser.me/api/portraits/men/61.jpg",
-      description:
-        "Creative designer focused on crafting user-friendly interfaces.",
-    },
-    {
-      name: "Ishita",
-      city: "Mumbai",
-      knows: "Yoga Basics",
-      wants: "Digital Marketing",
-      img: "https://randomuser.me/api/portraits/women/72.jpg",
-      description:
-        "Wellness enthusiast aiming to blend health and digital outreach.",
-    },
-    {
-      name: "Arjun",
-      city: "Chandigarh",
-      knows: "Photography",
-      wants: "Public Speaking",
-      img: "https://randomuser.me/api/portraits/men/55.jpg",
-      description: "Visual storyteller seeking to inspire through spoken word.",
-    },
-    {
-      name: "Priya",
-      city: "Hyderabad",
-      knows: "Content Writing, SEO",
-      wants: "Freelance Writing Projects",
-      img: "https://randomuser.me/api/portraits/women/44.jpg",
-      description:
-        "Wordsmith dedicated to creating impactful and engaging content.",
-    },
-    {
-      name: "Rohit",
-      city: "Pune",
-      knows: "Node.js, Express",
-      wants: "Back-End Development Jobs",
-      img: "https://randomuser.me/api/portraits/men/34.jpg",
-      description:
-        "Back-end developer passionate about building scalable systems.",
-    },
-    {
-      name: "Ananya",
-      city: "Kolkata",
-      knows: "Graphic Design, Photoshop",
-      wants: "Branding Projects",
-      img: "https://randomuser.me/api/portraits/women/36.jpg",
-      description:
-        "Design enthusiast who loves creating strong brand identities.",
-    },
-    {
-      name: "Kabir",
-      city: "Ahmedabad",
-      knows: "Python, Machine Learning",
-      wants: "AI Research Opportunities",
-      img: "https://randomuser.me/api/portraits/men/50.jpg",
-      description:
-        "Tech explorer fascinated by the future of artificial intelligence.",
-    },
-    {
-      name: "Meera",
-      city: "Jaipur",
-      knows: "Event Planning",
-      wants: "Wedding Planning Projects",
-      img: "https://randomuser.me/api/portraits/women/20.jpg",
-      description:
-        "Detail-oriented planner who loves crafting memorable events.",
-    },
-    {
-      name: "Aman",
-      city: "Lucknow",
-      knows: "Video Editing, Premiere Pro",
-      wants: "Film Editing Jobs",
-      img: "https://randomuser.me/api/portraits/men/23.jpg",
-      description:
-        "Creative editor passionate about storytelling through videos.",
-    },
-  ];
-
+  
   return (
     <>
       <NavBar />
@@ -111,12 +20,12 @@ function App() {
           path="/"
           element={
             <>
-              <UserCard users={users} />
-              <HowItWorks />
+              <UserCard />
+             
             </>
           }
         />
-        <Route path="/usercard" element={<UserCard users={users} />} />
+
         <Route
           path="/create-swap"
           element={
--- frontend/src/components/layout/UserCard.jsx
+++ frontend/src/components/layout/UserCard.jsx
@@ -1,37 +1,73 @@
 import React from "react";
+import HowItWorks from "./HowItWorks.jsx";
+import { useHttpClient } from "../../common/hooks/http-hook.js";
 
+const UserCard = () => {
+  const { sendRequest } = useHttpClient();
+  const [tasksList, setTasksList] = React.useState([]);
+  React.useEffect(() => {
+    const getAllTasks = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${import.meta.env.VITE_APP_BACKEND_URL}/tasks`
+        );
+        if (responseData) {
+          setTasksList(responseData.tasks)
+          console.log(responseData);
+          /* console.log(
+            responseData.tasks.map((task) =>
+              formatUTCToLocal(task.deadline, false)
 
-const UserCard = ({ users = [], onConnect }) => {
+            )
+          ); */
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getAllTasks();
+  }, []);
+
   return (
-    <section className="page ">
-      <h2 className="section_title">People on SkillBoard</h2>
-      <div className="user_list">
-        {users.map((user, index) => (
-          <div className="user_card" key={index}>
-            <img src={user.img} alt={user.name} className="profile_img" />
+    <>
+      <section className="page ">
+        <h2 className="section_title">People on SkillBoard</h2>
+        <div className="user_list">
+          {tasksList.map((singleTask, index) => (
+            <div className="user_card" key={index}>
+              <img
+                src={`${import.meta.env.VITE_APP_ASSET_URL}/${
+                  singleTask.creator.image
+                }`}
+                alt={singleTask.creator.name}
+                className="profile_img"
+              />
 
-            <div className="user_center">
-              <h3>{user.name}</h3>
-              <p className="city">{user.city}</p>
-              <div className="skills">
-                {user.knows.split(",").map((skill, i) => (
-                  <span key={i} className="skill_tag">
-                    {skill.trim()}
-                  </span>
-                ))}
+              <div className="user_center">
+                <h3>{singleTask.creator.name}</h3>
+                <p className="city">{singleTask.location}</p>
+                <div className="skills offeredSkill">
+                  {singleTask.offeredTask.map((singleOfferedTask, i) => (
+                    <span key={i} className="skill_tag">
+                      {singleOfferedTask.trim()}
+                    </span>
+                  ))}
+                </div>
+                <p className="description">{singleTask.description}</p>
               </div>
-              <p className="description">{user.description}</p>
-            </div>
 
-            <div className="user_right">
-              <button className="connect_btn" onClick={() => onConnect(user)}>
-                Connect
-              </button>
+              <div className="user_right">
+                <button className="connect_btn" onClick={() => {}}>
+                  Connect
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+      <HowItWorks />
+    </>
   );
 };
 
